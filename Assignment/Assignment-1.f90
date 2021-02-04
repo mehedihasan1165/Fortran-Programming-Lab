0fe6a7340c7f6545cf3f559536fd1676e5233b7b
Mehedi Hasan <bishad.00@gmail.com>
2021-02-05 01:19:04 +0600

    Update Assignment-1.f90

--- Assignment/Assignment-1.f90
+++ Assignment/Assignment-1.f90
@@ -4,15 +4,15 @@ program partialSum
     real :: ans = 0.0
     real j
     
-	print *, "Enter a number"
+    print *, "Enter a number"
     read *, n
     
-	do while (n < 1)
+    do while (n < 1)
         print *, "Enter a positive number"
         read *, n
     end do
     
-	do i = 1, n
+    do i = 1, n
         j = i
         ans = ans + (((j)/((j + 1)*(j + 1))))
     end do

83a5df85369740e6058d08cc1c08f211793286d7
Mehedi Hasan <bishad.00@gmail.com>
2021-02-05 01:17:15 +0600

    Update regressionElemental.f90

--- Lab/regressionElemental.f90
+++ Lab/regressionElemental.f90
@@ -16,19 +16,19 @@ program regression
     real m, c
     real xbar, ybar
     
-	open (10, file="points.dat") ! open the data file; attach to unit 10
+    open (10, file="points.dat") ! open the data file; attach to unit 10
     read (10, *) n
     allocate (x(n), y(n))
     read (10, *) (x(i), y(i), i=1, n)
     close (10) ! finished with the data file
     
-	!calculate the best-fit stright line
+    !calculate the best-fit stright line
     xbar = sum(x)/n
     ybar = sum(y)/n
     m = (sum(x*y)/n - xbar*ybar)/(sum(x*x)/n - xbar**2)
     c = ybar - m*xbar
     
-	print *, "Slope= ", m
+    print *, "Slope= ", m
     print *, "Intercept= ", c
     print "(3(1x,a10))", "x", "y", "mx+c"
     print "(3(1x,es10.3))", (x(i), y(i), m*x(i) + c, i=1, n)
